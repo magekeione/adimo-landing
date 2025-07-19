9fe4763df93facd8ee25b67548f41bdd549d88cd
ganaalex98 <gana.alex98@gmail.com>
2025-07-19 03:48:28 +0300

    6th

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/context/ThemeContext";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title:
-    "ADIMO - Soluție digitală pentru administrarea asociațiilor de locatari",
-  description:
-    "Platforma gratuită ADIMO simplifică gestionarea asociațiilor de locatari. Facturi, cheltuieli, comunicare - totul într-un singur loc.",
-};
 
 export default function RootLayout({
   children,
@@ -20,9 +9,7 @@ export default function RootLayout({
   return (
     <html lang="ro">
       <body suppressHydrationWarning>
-        <div className={inter.className}>
-          <ThemeProvider>{children}</ThemeProvider>
-        </div>
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );

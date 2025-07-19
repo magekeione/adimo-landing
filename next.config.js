968e5c175a27826fc8c92d3a714f9acb5e4e2db7
ganaalex98 <gana.alex98@gmail.com>
2025-07-19 03:15:33 +0300

    2

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     domains: ["via.placeholder.com"],
     formats: ["image/avif", "image/webp"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async generateBuildId() {
     return "adimo-v1";
   },
